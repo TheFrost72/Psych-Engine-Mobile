5d229bd8b89ccea7df5dd809717636699fb06f04
Frost <samuelteodoziolima2010@gmail.com>
2026-01-23 10:19:24 -0300

    Update PauseSubState.hx

--- source/substates/PauseSubState.hx
+++ source/substates/PauseSubState.hx
@@ -267,6 +267,7 @@ class PauseSubState extends MusicBeatSubstate
 			{
 				case "Resume":
 					Paths.clearUnusedMemory();
+					PlayState.instance.pauseButton.onResume();
 					close();
 				case 'Change Difficulty':
 					menuItems = difficultyChoices;
