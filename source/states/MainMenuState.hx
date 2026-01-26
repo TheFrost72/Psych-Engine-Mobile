6e395545b032c81fb5b063bb02190b6908093ec1
Frost <samuelteodoziolima2010@gmail.com>
2026-01-26 13:58:35 -0300

    Update MainMenuState.hx

--- source/states/MainMenuState.hx
+++ source/states/MainMenuState.hx
@@ -89,8 +89,9 @@ class MainMenuState extends MusicBeatState
 		var uiScale:Float = Math.min(scaleX, scaleY);
 		for (num => option in optionShit)
 		{
-			var item:FlxSprite = createMenuItem(option, 0, ((num * 140) + 90) * scaleY);
+			var item:FlxSprite = createMenuItem(option, 0, ((num * 160) + 90) * scaleY);
 			item.y += (4 - optionShit.length) * 70 * scaleY; // Offsets for when you have anything other than 4 items
+			item.x -= 10 * uiScale;
 			item.scale.set(uiScale, uiScale);
 			item.screenCenter(X);
 			item.updateHitbox();
