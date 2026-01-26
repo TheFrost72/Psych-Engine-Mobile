7652b409e2041948510b504cd09cc1abcf651d6f
Frost <samuelteodoziolima2010@gmail.com>
2026-01-26 08:21:45 -0300

    Update MainMenuState.hx

--- source/states/MainMenuState.hx
+++ source/states/MainMenuState.hx
@@ -14,11 +14,14 @@ enum MainMenuColumn {
 
 class MainMenuState extends MusicBeatState
 {
+	static inline var BASE_W:Int = 1280;
+	static inline var BASE_H:Int = 720;
 	public static var psychEngineVersion:String = '1.0.4'; // This is also used for Discord RPC
 	public static var curSelected:Int = 0;
 	public static var neuroEngineVersion:String = '0.1.0';
 	public static var curColumn:MainMenuColumn = CENTER;
 	var allowMouse:Bool = true; //Turn this off to block mouse movement in menus
+	var uiScale:Float = Math.min(FlxG.width / BASE_W, FlxG.height / BASE_H);
 
 	var menuItems:FlxTypedGroup<FlxSprite>;
 	var leftItem:FlxSprite;
@@ -84,8 +87,10 @@ class MainMenuState extends MusicBeatState
 
 		for (num => option in optionShit)
 		{
-			var item:FlxSprite = createMenuItem(option, 0, (num * 140) + 90);
-			item.y += (4 - optionShit.length) * 70; // Offsets for when you have anything other than 4 items
+			var extraY = (FlxG.height - 720 * uiScale) / 2;
+			var item:FlxSprite = createMenuItem(option, 0, ((num * 140) + 90) * uiScale + extraY);
+			item.y += (4 - optionShit.length) * 70 * uiScale; // Offsets for when you have anything other than 4 items
+			item.scale.set(uiScale, uiScale);
 			item.screenCenter(X);
 		}
 
