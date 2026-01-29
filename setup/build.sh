49325bc4398e5eec8f88bc961792a7df84649b0d
Frost <samuelteodoziolima2010@gmail.com>
2026-01-29 20:55:18 -0300

    Update build.sh

--- setup/build.sh
+++ setup/build.sh
@@ -9,7 +9,7 @@ echo "This might take a few moments depending on your internet speed."
 haxelib install hxcpp 4.3.2 --skip-dependencies --quiet
 haxelib git lime https://github.com/Neuro-Engine-Team/lime --skip-dependencies --quiet
 haxelib git openfl https://github.com/Neuro-Engine-Team/openfl --skip-dependencies --quiet
-haxelib git https://github.com/Neuro-Engine-Team/flixel --skip-dependencies --quiet
+haxelib git flixel https://github.com/Neuro-Engine-Team/flixel --skip-dependencies --quiet
 haxelib install flixel-addons 3.2.2 --skip-dependencies --quiet
 haxelib install flixel-tools 1.5.1 --skip-dependencies --quiet
 
